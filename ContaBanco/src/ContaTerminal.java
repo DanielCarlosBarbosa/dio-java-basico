2ac0bcade34d907f8ee4f019883d201f18622a58
Daniel Carlos Barbosa <daniel.carlos.barbosa@outlook.com>
2023-09-26 10:09:32 -0300

    Utilização do scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TODO: Conhecer e importar a classe Scanner
+        //Exibir as mensagens para o usuário
+        //Obter pela scanner os calores digitados no terminal 
+        Scanner ler = new Scanner(System.in);
+            System.out.println("Por favor, digite o nome do cliente");
+            String nome_cliente = ler.next();//"MARIO ANDRADE";
+
+            System.out.println("Por favor, digite a agência");
+            String agencia = ler.next();//"067-8";
+
+            System.out.println("Por favor, digite o número da conta");
+            int conta = ler.nextInt();//1021;
+
+            System.out.println("Por favor, digite o saldo");
+            double saldo = ler.nextDouble();//237.48;
+
+            // "Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque"
+            String msg = "Olá "+ nome_cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque";
+
+            System.out.println(msg);
+
     }
 }
